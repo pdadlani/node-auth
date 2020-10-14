eaf23eddbd7bd4ac7f35352dd51a83dc3b0ed722
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2020-10-14 14:07:25 -0700

    unique username error handling.

--- routes/auth.js
+++ routes/auth.js
@@ -12,6 +12,13 @@ router.post('/register', async (req, res) => {
     return res.status(400).send(error.details[0].message);
   }
 
+  // Check if user is already in the database
+  const usernameExist = await User.findOne({username: req.body.username});
+  if (usernameExist) {
+    return res.status(400).send("Username already exists.");
+  };
+
+  // Create a new user
   const user = new User({
     username: req.body.username,
     password: req.body.password
